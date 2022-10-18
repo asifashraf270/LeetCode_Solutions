3bf163638ab7c3b055d487eab056dbc6bb52fb4d
asif <muhammadasifashraf270@gmail.com>
2022-10-18 16:12:38 +0400

    LeetCode Problem 3

---
+++ src/problem3.java
@@ -0,0 +1,41 @@
+public class problem3 {
+
+    public static void main(String[] args){
+        String[] input={"flower","flow","flight"};
+        System.out.println(longestPrefix(input));
+    }
+
+
+    public static String longestPrefix(String[] strs){
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
+
+
+
+   /* public static String longestPrefix(String[] strs){
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while(c < first.length())
+        {
+            if (first.charAt(c) == last.charAt(c))
+                c++;
+            else
+                break;
+        }
+        return c == 0 ? "" : first.substring(0, c);
+    }*/
+}
